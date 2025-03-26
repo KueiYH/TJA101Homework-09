d2abf093cbdfac70dff1efb2db9c743aed936fe7
Akuei <akueiyh@gmail.com>
2025-03-26 16:06:02 +0800

    Homework 1 & 2

---
+++ src/hw1/HomeWork1.java
@@ -0,0 +1,48 @@
+package hw1;
+
+public class HomeWork1 {
+	public static void main(String[] args) {
+		
+		//Q1
+		System.out.print("Q1. ");
+		int a = 6;
+		int b = 12;
+		System.out.println("和:" + (a + b) + "," + "積:" + (a * b));
+		//Q2
+		System.out.print("Q2. ");
+		int egg = 200;
+		System.out.println("200顆蛋是" + egg / 12 + "打" + 200 % 12 + "顆" );
+		//Q3
+		System.out.print("Q3. ");
+		int totalSecs = 256559;
+		int days = totalSecs / 86400;
+		int hours = (totalSecs % 86400) / 3600;
+		int mins = ((totalSecs % 86400) % 3600) / 60;
+		int secs = ((totalSecs % 86400) % 3600) % 60;
+		System.out.println("共" + days + "天" + hours + "小時" + mins + "分" + secs + "秒");
+		//Q4
+		System.out.print("Q4. ");
+		float pi = 3.1415f;
+		float radius = 5;
+		float area = radius * radius * pi;
+		float perimeter = 2 * radius * pi;
+		System.out.println("面積:" + area + " 周長:" + perimeter);
+		//Q5
+//		本利和= 本金× (1 + 年利率) ^ 期間
+		System.out.print("Q5. ");
+		double principal = 1500000;
+		double interest = 0.02;
+		for (int i = 0; i <= 10; i ++) {
+			principal = principal * (1 + 0.02);
+		}
+		double sum = principal;
+		
+		System.out.println("十年過後總金額為:"+ sum + "元" );
+		
+		//Q6
+		System.out.print("Q6. ");
+		System.out.println(5 + 5 + "，兩個5都沒有單引或雙引號，Java視作數字相加");
+		System.out.println(5 + '5' + "，因為字元符5的unicode碼為0035，十六進制0035轉為十進制為53，整數53 + 5等於58");
+		System.out.println(5 + "5" + "，因為整數與string相加會變成文字串接");
+	}
+}
---
+++ src/hw2/TestNineNine.java
@@ -0,0 +1,45 @@
+package hw2;
+
+public class TestNineNine {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//				System.out.print( i + "*" + j + "=" + i * j + "\t");
+//			}
+//			System.out.println();
+//		}
+//////// for...while
+//		for (int i = 1; i <10; i++) {
+//			int j = 1;
+//			while(j < 10) {
+//				System.out.print( i + "*" + j + "=" + i * j + "\t");
+//				j++;
+//			}
+//			System.out.println();
+//		}
+//////// for do...while
+//		for (int i = 1; i < 10; i++) {
+//			int j = 1;
+//			do {
+//				System.out.print( i + "*" + j + "=" + i * j + "\t");
+//				j ++;
+//				} while (j < 10); 
+//
+//					System.out.println();
+//			}
+
+//////// while do...while
+//		int i = 1;
+//		while (i < 10) {
+//			int j = 1;
+//			do { 
+//				System.out.print( i + "*" + j + "=" + i * j + "\t");
+//				j++;
+//			} while (j < 10);
+//			i ++;
+//			System.out.println();
+//		}
+	}
+}
